88be3277402380c2348d6dc61cd0dc7bdffd6874
jch <jch@FreeBSD.org>
2015-08-08 08:40:36 +0000

    Cherry-picked from HEAD:
    Notes:
        svn path=/head/; revision=286443
    
    Fix a kernel assertion issue introduced with r286227:
    Avoid too strict INP_INFO_RLOCK_ASSERT checks due to
    tcp_notify() being called from in6_pcbnotify().
    
    Reported by:	Larry Rosenman <ler@lerctr.org>
    Submitted by:	markj, jch
    
    (cherry picked from commit 349429fe8270f2579de42235749bb5ed010ee83a)

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -893,7 +893,7 @@ tcp_drop(struct tcpcb *tp, int errno)
 {
 	struct socket *so = tp->t_inpcb->inp_socket;
 
-	INP_INFO_RLOCK_ASSERT(&V_tcbinfo);
+	INP_INFO_LOCK_ASSERT(&V_tcbinfo);
 	INP_WLOCK_ASSERT(tp->t_inpcb);
 
 	if (TCPS_HAVERCVDSYN(tp->t_state)) {
@@ -1095,7 +1095,7 @@ tcp_close(struct tcpcb *tp)
 	struct inpcb *inp = tp->t_inpcb;
 	struct socket *so;
 
-	INP_INFO_RLOCK_ASSERT(&V_tcbinfo);
+	INP_INFO_LOCK_ASSERT(&V_tcbinfo);
 	INP_WLOCK_ASSERT(inp);
 
 #ifdef TCP_OFFLOAD
@@ -1173,7 +1173,7 @@ tcp_notify(struct inpcb *inp, int error)
 {
 	struct tcpcb *tp;
 
-	INP_INFO_RLOCK_ASSERT(&V_tcbinfo);
+	INP_INFO_LOCK_ASSERT(&V_tcbinfo);
 	INP_WLOCK_ASSERT(inp);
 
 	if ((inp->inp_flags & INP_TIMEWAIT) ||
